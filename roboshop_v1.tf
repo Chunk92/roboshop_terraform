56534044b8f1987241ecc16821df8013257980d6
Chanakya <chanakyadba92@gmail.com>
2026-06-10 16:47:19 +0530

    added ""

--- roboshop_v1.tf
+++ roboshop_v1.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test" {
-  ami           = ami-0b4f379183e5706b9
+  ami           = "ami-0b4f379183e5706b9"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0258fa7879ac0c14b"]
 
