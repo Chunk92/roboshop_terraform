ccee21b66a1de2340f6a4e85eb4ad215caea3586
Chanakya <chanakyadba92@gmail.com>
2026-06-16 12:53:18 +0530

    added resource

--- module/child_module/data.tf
+++ module/child_module/data.tf
@@ -6,3 +6,10 @@ data "aws_ami_ids" "ami" {
 output "ami_ids" {
   value = data.aws_ami_ids.ami
 }
+
+resource "aws_instance" "fuckyou" {
+    ami = data.aws_ami_ids.ami.id
+    instance_type = t3.micro
+}
+
+  
